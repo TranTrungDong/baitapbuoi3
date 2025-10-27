e4f1390fa21b8e3afb5099504d867ab58a1866fa
TranTrungDong <trungdong0702@gmail.com>
2025-10-27 09:12:11 +0700

    ket qua

--- Source.cpp
+++ Source.cpp
@@ -9,6 +9,41 @@ int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
 	cout <<"user1";
+
+int tong(int a, int b) {
+    return a + b;
+}
+int hieu(int a, int b) {
+    return a - b;
+}
+int tich(int a, int b) {
+    return a * b;
+}
+//Ép kiểu (float) để đảm bảo kết quả là số thực
+float thuong(int a, int b) {
+    if (b == 0) {
+        cout << "\nLoi: Khong the chia cho 0!" << endl;
+        return 0.0;
+    }
+    return (float)a / b;
+}
+
+// Hàm chính của chương trình
+int main() {
+    cout << "DAY LA CHUONG TRINH DE THUC HANH GIT" << endl;
+    cout << "user1" << endl;
+
+    int num1 = 10;
+    int num2 = 5;
+
+    cout << "-----------------------------------" << endl;
+    cout << "Ket qua phep tinh:" << endl;
+    cout << num1 << " + " << num2 << " = " << tong(num1, num2) << endl;
+    cout << num1 << " - " << num2 << " = " << hieu(num1, num2) << endl;
+    cout << num1 << " * " << num2 << " = " << tich(num1, num2) << endl;
+    cout << num1 << " / " << num2 << " = " << thuong(num1, num2) << endl;
+	cout << "-----------------------------------" << endl;
+    
 	system("pause");
 	return 0;
 }
